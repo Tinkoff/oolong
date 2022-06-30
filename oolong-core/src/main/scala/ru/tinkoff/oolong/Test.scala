f47c6d23d4b8ae408f1fa536772de0f1ce1a0499
Denis Savitsky <d.e.savitsky@gmail.com>
2022-06-30 15:01:54 +0300

    meta test

--- oolong-core/src/main/scala/ru/tinkoff/oolong/Test.scala
+++ oolong-core/src/main/scala/ru/tinkoff/oolong/Test.scala
@@ -3,4 +3,10 @@ package ru.tinkoff.oolong
 case class C(a: Int)
 case class Test(a: Int, b: Long, c: C)
 
-@main def run = println(Meta.meta[Test](_.a -> "aaa", _.b -> "bbb", _.c -> "ccc"))
+object Test extends App {
+  inline given QueryMeta[C] = Meta.meta[C](_.a -> "aaaaaa")
+  given QueryMeta[Test] = Meta.meta[Test](_.a -> "aaa", _.b -> "bbb", _.c -> "ccc")
+
+  Meta.meta[Test](_.a -> "aaa", _.b -> "bbb", _.c -> "ccc").fun.keySet.head
+
+}
